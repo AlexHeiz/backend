927f36a20941382db52760edec7e2d2d54cb4dba
Alex_Heiz <sania-l@bk.ru>
2022-08-13 03:35:35 +0300

    end

--- backend_api/urls.py
+++ backend_api/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tasks/', include('tasks.urls')),
-    path('api/', include('myapi.urls')),
+    path('api/', include('api.urls')),
 ]
